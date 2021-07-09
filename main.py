2ecb70ce4e9c0fe46aeef548258e00cf51cf0dfc
hunggithub21 <87210711+hunggithub21@users.noreply.github.com>
2021-07-09 15:41:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print('hello world!')
+python_set = {'banana', 'orange', 'grape', 'strawberry'}
+python_list = ['Michael', 'Chase', 'Audrey', 'Sophie']
+
+for elm in python_set:
+  print(f, "the fruit {python_set[elm]}")
